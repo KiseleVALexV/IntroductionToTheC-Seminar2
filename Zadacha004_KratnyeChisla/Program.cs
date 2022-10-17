c4718fc15b0e6e01e2f18d04016426d0525b66ca
Alexey <manoil77@list.ru>
2022-10-17 21:54:50 +0300

    Add Zadacha004 Kratnye Chisla

---
+++ Zadacha004_KratnyeChisla/Program.cs
@@ -0,0 +1,22 @@
+﻿// Установить является ли число2 кратным к числу1. Если нет, вывести остаток.
+// 34, 5 - не кратно, остаток 4
+// 16, 4 - кратно
+
+Console.WriteLine("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int resault = number1 % number2;
+
+if (resault == 0)
+{
+    Console.WriteLine($"Число {number2} является кратным к числу {number1}");
+}
+else
+{
+Console.WriteLine($"Остаток: {resault}");
+}
+
+Console.ReadKey();
