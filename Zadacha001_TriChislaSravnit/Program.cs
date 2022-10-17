51955644af77e41ad236e9fee7eeb71db2fbfecc
Alexey <manoil77@list.ru>
2022-10-17 20:35:08 +0300

    Add Zadacha001_TriChislaSravnit

---
+++ Zadacha001_TriChislaSravnit/Program.cs
@@ -0,0 +1,23 @@
+﻿// Найти мах из трех чисел
+Console.WriteLine("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число C: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (b>max)
+{
+    max=b;
+}
+
+if (c>max)
+{
+    max=c;
+}
+
+Console.WriteLine($"Максимальное из трех чисел: {max}");
+Console.ReadKey();
