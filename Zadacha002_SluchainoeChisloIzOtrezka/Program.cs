5f9ea67811b0778c2f805ff04781d4c0eab4f27c
Alexey <manoil77@list.ru>
2022-10-17 23:36:27 +0300

    Add Zadacha002 o

---
+++ Zadacha002_SluchainoeChisloIzOtrezka/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая.
+int number = new random().Next(10,100);
+int a1 = number / 10;
+int a2 = number % 10;
+
+Console.WriteLine(number);
+
+if (a1 > a2)
+{
+    Console.WriteLine($"Наибольшая цифра в числе: {a1}");
+}
+if (a2 > a1)
+{
+    Console.WriteLine($"Наибольшая цифра в числе: {a2}");
+}
+if (a1 == a2)
+{
+    Console.WriteLine($"Цифры в числе равны");
+}
+
+Console.ReadKey();
