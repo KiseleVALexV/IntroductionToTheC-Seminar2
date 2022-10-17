f1e0348ee7fd4215da7b728a6ff97054c7520c23
Alexey <manoil77@list.ru>
2022-10-17 22:36:56 +0300

    Add Zadacha003 Udalenie vtoroi cyfry chisla

---
+++ Zadacha003_UdalenieVtoroiCifryChisla/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая убирает вторую цифру трехзначного случайного числа.
+
+int number = new Random().Next(100,999);
+Console.WriteLine($"Случайное число : {number}");
+
+// Console.WriteLine("Введите число : ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+int resault = (number / 100) * 10 + number % 10;
+
+Console.WriteLine($"Результат: {resault}");
+
+Console.ReadKey();
